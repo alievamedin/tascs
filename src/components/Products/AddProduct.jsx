09a84d4fa3cc937979b6815cd71ad0666e30b636
medina <alievamedina@gmail.com>
2022-12-21 18:18:49 +0600

    second commit

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from '@mui/material';
 import React, { useContext, useState } from 'react';
 import { productsContext, useProducts } from '../../contexts/ProductsContext';
 import ProductsPage from '../../pages/ProductsPage';
@@ -19,12 +20,17 @@ const AddProduct = () => {
 
     return (
         <div className='inputs'>
-            <input className='inp' type="text" placeholder='name' value={products.name}    onChange={(e) => setProducts({...products, name : e.target.value})}  />
-            <input  className='inp' type="text" placeholder='type'  value={products.type}     onChange={(e) => setProducts({...products, type : e.target.value})}/>
-            <input  className='inp' type="text" placeholder='description'  value={products.description}   onChange={(e) => setProducts({...products, description : e.target.value})} />
-            <input  className='inp' type="text" placeholder='price'  value={products.price}   onChange={(e) => setProducts({...products, price : e.target.value})}/>
-            <input  className='inp' type="text" placeholder='img'  value={products.img}  onChange={(e) => setProducts({...products, img: e.target.value})} />
-            <button  className='inp' onClick={()=>addProducts(products)}>Add</button>
+           
+
+            <Button style={{margin:"15px"}} variant="contained"  onClick={()=>addProducts(products)}>Contained</Button>
+            <TextField id="filled-basic" placeholder="name"   value={products.name}  onChange={(e) => setProducts({...products, name: e.target.value})} />
+            <TextField id="filled-basic" placeholder="type" value={products.type}  onChange={(e) => setProducts({...products, type: e.target.value})} />
+            <TextField id="filled-basic" placeholder="description"    value={products.description}  onChange={(e) => setProducts({...products, description: e.target.value})} />
+
+            <TextField id="filled-basic" placeholder="price"   value={products.price}  onChange={(e) => setProducts({...products, price: e.target.value})} />
+
+            <TextField id="filled-basic" placeholder="img"   value={products.img}  onChange={(e) => setProducts({...products, img: e.target.value})} />
+            {/* <button  className='inp' onClick={()=>addProducts(products)}>Add</button> */}
         </div>
     );
 };
